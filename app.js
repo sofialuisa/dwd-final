9c6d4130942f8b51d0bbc043e6046df850217d95
Sofía Suazo <sls936@nyu.edu>
2019-05-11 14:30:02 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+const url = require('url')
+const path = require('path')
+//var mustacheExpress = require('mustache-express');
+//var request = require('request');
+const PORT = process.env.PORT || 8000;
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', function(req, res){
+      res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+app.listen(PORT, function() {
+});
