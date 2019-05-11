2e469c5c14a76477988b431e3680529b4996b24d
Sofía Suazo <sls936@nyu.edu>
2019-05-11 18:29:51 -0400

    pushin to heroku w/mustache

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 const url = require('url')
 const path = require('path')
-//var mustacheExpress = require('mustache-express');
+var mustacheExpress = require('mustache-express');
 //var request = require('request');
 const PORT = process.env.PORT || 8000;
 //Here we are configuring express to use body-parser as middle-ware.
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
       res.sendFile(path.join(__dirname + '/index.html'))
-      
+
 });
 
 app.listen(PORT, function() {
